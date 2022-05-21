f0c4349182c9f4eaf620da1678c7dc02fd425c83
Chris Vince <chrisvince@gmail.com>
2022-05-21 12:59:58 +0700

    feat: added initFirebase util
    Added initFirebase util which will init Firebase if it hasn't already
    been initialized.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app'
 import initAuth from '../utils/initAuth'
+import initFirebase from '../utils/initFirebase'
 
+initFirebase()
 initAuth()
 
 function MyApp({ Component, pageProps }: AppProps) {
