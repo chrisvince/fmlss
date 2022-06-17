fe4a4c2b563c838632ab6a26a590846e7f1c2871
Chris Vince <chrisvince@gmail.com>
2022-06-17 15:50:59 -0400

    fix: moved firebase functions directory
    Moved to prevent from conflicting with Netlify functions.

--- functions/src/checkUserHasPassword.ts
+++
@@ -1,8 +0,0 @@
-import * as functions from 'firebase-functions'
-import checkUserHasPasswordUtil from './auth/checkUserHasPassword'
-
-export const checkUserHasPassword = functions.https.onCall(async (data) => {
-  const { uid } = data
-  const userHasPassword = await checkUserHasPasswordUtil(uid)
-  return userHasPassword
-})
--- functions/src/util/generateRandomKey.ts
+++
@@ -1,5 +0,0 @@
-import { randomBytes } from 'crypto'
-
-const generateRandomKey = () => randomBytes(64).toString('hex')
-
-export default generateRandomKey
