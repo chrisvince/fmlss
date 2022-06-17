e9070a35503b5a9b648ec1859bd3e2a70fb05a0c
Chris Vince <chrisvince@gmail.com>
2022-06-17 18:07:59 -0400

    fix: fixed error reporting in api functions

--- pages/api/auth/signIn.ts
+++ pages/api/auth/signIn.ts
@@ -11,8 +11,8 @@ const handler = async (
 ) => {
   try {
     await setAuthCookies(req, res)
-  } catch (e) {
-    return res.status(500).json({ error: 'Unexpected error.' })
+  } catch (error) {
+    return res.status(500).json({ error })
   }
   return res.status(200).json({ success: true })
 }
