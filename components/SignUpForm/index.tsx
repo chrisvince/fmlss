c87c211486a0cbaf7f347e071ee6472630c67616
Chris Vince <chrisvince@gmail.com>
2022-06-24 09:15:55 -0400

    feat: added error messaging on sign up page

--- components/SignUpForm/index.tsx
+++ components/SignUpForm/index.tsx
@@ -2,14 +2,16 @@ import React, { SyntheticEvent, useState } from 'react'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import GoogleAuthButton from '../GoogleAuthButton'
+import Link from 'next/link'
 
 const auth = firebase.auth()
 
 const UI_STATES = {
-  NOT_SUBMITTED: 'not-submitted',
-  LOADING: 'loading',
-  SUBMITTED: 'submitted',
+  EMAIL_IN_USE_ERROR: 'email-in-use-error',
   ERROR: 'error',
+  LOADING: 'loading',
+  NOT_SUBMITTED: 'not-submitted',
+  SUBMITTED: 'submitted',
 }
 
 const EMAIL_ID = 'email'
@@ -20,6 +22,7 @@ const PASSWORD_LABEL = 'Password'
 
 const SignUpForm = () => {
   const [uiState, setUiState] = useState(UI_STATES.NOT_SUBMITTED)
+  const [errorMessage, setErrorMessage] = useState<string | null>(null)
 
   const onSubmit = async (event: SyntheticEvent) => {
     const formData = new FormData(event.target as HTMLFormElement)
@@ -27,13 +30,21 @@ const SignUpForm = () => {
     const password = formData.get(PASSWORD_ID) as string | undefined
 
     try {
+      setErrorMessage(null)
       setUiState(UI_STATES.LOADING)
       await auth.createUserWithEmailAndPassword(
         email as string,
         password as string,
       )
       setUiState(UI_STATES.SUBMITTED)
-    } catch (error) {
+    } catch (error: any) {
+      if (error.code === 'auth/email-already-in-use') {
+        setUiState(UI_STATES.EMAIL_IN_USE_ERROR)
+        return
+      }
+      setErrorMessage(
+        error.message ?? 'There was an error. Please try again later.'
+      )
       setUiState(UI_STATES.ERROR)
       console.error(error)
     }
@@ -64,6 +75,15 @@ const SignUpForm = () => {
         <button type="submit">
           Sign up
         </button>
+        {uiState === UI_STATES.EMAIL_IN_USE_ERROR && (
+          <p>
+            The email is already in use. Please{' '}
+            <Link href="/"><a>Sign in</a></Link>
+          </p>
+        )}
+        {uiState === UI_STATES.ERROR && (
+          <p>{errorMessage}</p>
+        )}
       </form>
       <GoogleAuthButton
         mode="signUp"
