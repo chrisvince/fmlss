2a2b6ca0788cc500078154f02781fe06966fa853
Chris Vince <chrisvince@gmail.com>
2022-07-12 14:17:44 -0400

    chore: removed optional chaining from  mappers

--- utils/mapHashtagDocToData.ts
+++ utils/mapHashtagDocToData.ts
@@ -3,13 +3,13 @@ import type { FirebaseDoc, HashtagData } from '../types'
 type MapHashtagDocToData = (postDoc: FirebaseDoc) => HashtagData
 
 const mapHashtagDocToData: MapHashtagDocToData = doc => {
-  const data = doc.data()
+  const data = doc.data()!
   return {
-    createdAt: data?.createdAt?.toMillis() as string,
-    hashtag: data?.hashtag as string,
+    createdAt: data.createdAt.toMillis() as string,
+    hashtag: data.hashtag as string,
     id: doc.id as string,
-    updatedAt: data?.updatedAt?.toMillis() as string,
-    usageCount: data?.usageCount as number,
+    updatedAt: data.updatedAt.toMillis() as string,
+    usageCount: data.usageCount as number,
   }
 }
 
--- utils/mapPostDocToData.ts
+++ utils/mapPostDocToData.ts
@@ -4,17 +4,17 @@ import { getParentIdFromDbReference } from './dbReferences'
 type MapPostDbToClient = (postDoc: FirebaseDoc) => PostData
 
 const mapPostDocToData: MapPostDbToClient = postDoc => {
-  const postData = postDoc.data()
+  const postData = postDoc.data()!
   return {
-    body: postData?.body as string,
-    createdAt: postData?.createdAt?.toMillis() as string,
+    body: postData.body as string,
+    createdAt: postData.createdAt.toMillis() as string,
     id: postDoc.id as string,
-    likesCount: postData?.likesCount ?? 0 as number,
+    likesCount: postData.likesCount as number,
     parentId: getParentIdFromDbReference(postDoc.ref.path) as string,
-    postsCount: postData?.postsCount ?? 0 as number,
+    postsCount: postData.postsCount as number,
     reference: postDoc.ref.path as string,
-    slug: postData?.slug as string,
-    updatedAt: postData?.updatedAt?.toMillis() as string,
+    slug: postData.slug as string,
+    updatedAt: postData.updatedAt.toMillis() as string,
   }
 }
 
