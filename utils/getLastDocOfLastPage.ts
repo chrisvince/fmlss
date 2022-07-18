4f42f79ce52ff5dd460ff6995f91ca2ee10c79ea
Chris Vince <chrisvince@gmail.com>
2022-07-17 20:05:41 -0400

    chore: updated typing

--- utils/getLastDocOfLastPage.ts
+++ utils/getLastDocOfLastPage.ts
@@ -1,7 +1,6 @@
 import { Category, Hashtag, Post } from '../types'
 
 const getLastDocOfLastPage =
-  (data: (Category | Hashtag | Post)[][] | undefined) =>
-    data?.at?.(-1)?.at?.(-1)?.doc
+  (data: any[][] | undefined) => data?.at?.(-1)?.at?.(-1)?.doc
 
 export default getLastDocOfLastPage
