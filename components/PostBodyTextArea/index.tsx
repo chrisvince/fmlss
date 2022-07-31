617164de6140889c4b2d1fe1dc8ec3108e51e5cf
Chris Vince <chrisvince@gmail.com>
2022-07-31 12:06:49 -0400

    feat: updated numbr formatting
    Updated number formatting to abbreviate above 9999.

--- components/PostBodyTextArea/index.tsx
+++ components/PostBodyTextArea/index.tsx
@@ -1,30 +1,39 @@
-import { forwardRef, useEffect, useImperativeHandle, useState } from 'react'
+import {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react'
 import {
   Editor,
   EditorState,
   convertFromRaw,
   CompositeDecorator,
+  convertFromHTML,
+  ContentState,
+  SelectionState,
 } from 'draft-js'
-import 'draft-js/dist/Draft.css'
-import classnames from 'classnames'
-import styles from './index.module.scss'
+import 'draft-js/dist/Draft.css' // check if needed
+import { Typography, useTheme } from '@mui/material'
+
 import constants from '../../constants'
 
 const { HASHTAG_REGEX } = constants
 
-const initialEditorState = convertFromRaw({
-  entityMap: {},
-  blocks: [
-    {
-      text: '',
-      key: 'foo',
-      type: 'unstyled',
-      entityRanges: [],
-      depth: 0,
-      inlineStyleRanges: [],
-    },
-  ],
-})
+// const initialEditorState = convertFromRaw({
+//   entityMap: {},
+//   blocks: [
+//     {
+//       text: '',
+//       key: '',
+//       type: 'unstyled',
+//       entityRanges: [],
+//       depth: 0,
+//       inlineStyleRanges: [],
+//     },
+//   ],
+// })
 
 type DraftStrategyCallback = (start: number, end: number) => void
 
@@ -49,56 +58,110 @@ const createRegexDecorator = (
 })
 
 const Hashtag = ({ children }: { children: string }) => (
-  <span style={{ color: 'dodgerblue' }}>{children}</span>
+  <Typography
+    color="secondary"
+    component="span"
+  >
+    {children}
+  </Typography>
 )
 
 const compositeDecorator = new CompositeDecorator([
   createRegexDecorator(HASHTAG_REGEX, Hashtag),
 ])
 
-type PropTypes = {
+type Props = {
   disabled?: boolean
+  focusOnMount?: boolean
   onChange?: (text: string) => void
+  value?: string
 }
 
 const PostBodyTextArea = (
-  { disabled, onChange }: PropTypes,
-  ref: React.Ref<{ clear: () => void }>
+  {
+    disabled,
+    focusOnMount,
+    onChange,
+    value = '',
+  }: Props,
 ) => {
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createWithContent(initialEditorState, compositeDecorator)
-  )
-
-  useEffect(() => {
-    onChange?.(editorState.getCurrentContent().getPlainText())
-  }, [editorState, onChange])
-
-  const clear = () => {
-    const cleanState = EditorState.createWithContent(
-      initialEditorState,
+  const [editorState, setEditorState] = useState(() => {
+    const blocksFromHTML = convertFromHTML(value)
+    const contentState = ContentState.createFromBlockArray(
+      blocksFromHTML.contentBlocks,
+      blocksFromHTML.entityMap
+    )
+    const editorState = EditorState.createWithContent(
+      contentState,
       compositeDecorator
     )
-    setEditorState(cleanState)
-  }
-
-  useImperativeHandle(ref, () => ({ clear }))
-
-  const onEditorStateChange = (editorState: EditorState) => setEditorState(editorState)
-
-  const wrapperClassName = classnames(styles.wrapper, {
-    [styles.disabled]: disabled,
+    return editorState
   })
+  const editorRef = useRef<Editor>()
+  const { palette } = useTheme()
+
+  useEffect(() => {
+    const blocksFromHTML = convertFromHTML(value)
+
+    const contentState = ContentState.createFromBlockArray(
+      blocksFromHTML.contentBlocks,
+      blocksFromHTML.entityMap
+    )
+
+    const editorState = EditorState.createWithContent(
+      contentState,
+      compositeDecorator
+    )
+
+    setEditorState(editorState)
+  }, [value])
+
+  const currentText = editorState.getCurrentContent().getPlainText()
+  useEffect(() => {
+    onChange?.(currentText)
+  }, [currentText, onChange])
+
+  useEffect(() => {
+    if (!focusOnMount) return
+    editorRef.current?.focus()
+  }, [focusOnMount])
+
+  // const clear = () => {
+  //   const content = convertFromHTML('')
+  //   const contentState = ContentState.createFromBlockArray(
+  //     content.contentBlocks,
+  //     content.entityMap
+  //   )
+  //   const cleanState = EditorState.createWithContent(
+  //     contentState,
+  //     compositeDecorator
+  //   )
+  //   setEditorState(cleanState)
+  // }
+
+  // useImperativeHandle(ref, () => ({ clear }))
+
+  const onEditorStateChange = (editorState: EditorState) =>
+    setEditorState(editorState)
 
   return (
-    <div className={wrapperClassName}>
+    <Typography
+      component="div"
+      variant="body1"
+      sx={{ opacity: disabled ? palette.action.disabledOpacity : 1 }}
+    >
       <Editor
+        // @ts-ignore
+        ref={editorRef}
+        preserveSelectionOnBlur={true}
         editorKey="editor"
         editorState={editorState}
         onChange={onEditorStateChange}
         readOnly={disabled}
+        placeholder="Write a reply"
       />
-    </div>
+    </Typography>
   )
 }
 
-export default forwardRef(PostBodyTextArea)
+export default PostBodyTextArea
--- utils/formatting/formatCount.ts
+++ utils/formatting/formatCount.ts
@@ -1,5 +1,10 @@
 import numeral from 'numeral'
 
-const formatCount = (number: number) => numeral(number).format('0,0')
+const formatCount = (number: number) => {
+  if (number > 9999) {
+    return numeral(number).format('0[.]0a').toUpperCase()
+  }
+  return numeral(number).format('0,0')
+}
 
 export default formatCount
