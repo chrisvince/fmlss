e7fc581f16964ec611c95293df3471a5eadff971
Chris Vince <chrisvince@gmail.com>
2022-08-04 00:24:54 -0400

    fix: added container to new post form

--- components/NewPostForm/index.tsx
+++ components/NewPostForm/index.tsx
@@ -5,6 +5,7 @@ import { SyntheticEvent, useRef, useState } from 'react'
 import PostBodyTextArea, { PostBodyTextAreaRef } from '../PostBodyTextArea'
 import { LoadingButton } from '@mui/lab'
 import useCreatePost from '../../utils/data/post/useCreatePost'
+import MobileContainer from '../MobileContainer'
 
 const NewPostForm = () => {
   const postBodyTextAreaRef = useRef<PostBodyTextAreaRef>(null)
@@ -26,42 +27,44 @@ const NewPostForm = () => {
   }
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit}
-    >
-      <Box>
-        <PostBodyTextArea
-          disabled={isLoading}
-          focusOnMount
-          onChange={handleTextChange}
-          onCommandEnter={submitPost}
-          placeholder="What's on your mind?"
-          ref={postBodyTextAreaRef}
-          username="chrisvince"
-        />
-        {errorMessage && (
-          <Typography variant="caption" color="error">
-            {errorMessage}
-          </Typography>
-        )}
-      </Box>
+    <MobileContainer>
       <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'flex-end',
-        }}
+        component="form"
+        onSubmit={handleSubmit}
       >
-        <LoadingButton
-          type="submit"
-          variant="contained"
-          loading={isLoading}
-          disabled={!hasContent}
+        <Box>
+          <PostBodyTextArea
+            disabled={isLoading}
+            focusOnMount
+            onChange={handleTextChange}
+            onCommandEnter={submitPost}
+            placeholder="What's on your mind?"
+            ref={postBodyTextAreaRef}
+            username="chrisvince"
+          />
+          {errorMessage && (
+            <Typography variant="caption" color="error">
+              {errorMessage}
+            </Typography>
+          )}
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+          }}
         >
-          Submit
-        </LoadingButton>
+          <LoadingButton
+            type="submit"
+            variant="contained"
+            loading={isLoading}
+            disabled={!hasContent}
+          >
+            Submit
+          </LoadingButton>
+        </Box>
       </Box>
-    </Box>
+    </MobileContainer>
   )
 }
 
