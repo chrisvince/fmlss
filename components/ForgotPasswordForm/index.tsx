5de6ae90dfe04ee589f9994acd8afd91f43db7c5
Chris Vince <chrisvince@gmail.com>
2022-09-29 12:27:12 -0400

    chore: updated forgot password page and form
    Updated so that title isn't shown at all times but only when form is
    shown.

--- components/ForgotPasswordForm/index.tsx
+++ components/ForgotPasswordForm/index.tsx
@@ -7,10 +7,12 @@ import constants from '../../constants'
 import { Button, Divider, TextField, Typography } from '@mui/material'
 import { LoadingButton } from '@mui/lab'
 import Link from 'next/link'
+import TextAndCta from '../TextAndCta'
 
 const { FORM_MESSAGING, EMAIL_REGEX_PATTERN } = constants
 
 const EMAIL_ID = 'email'
+const EMAIL_LABEL = 'Email'
 
 const ForgotPasswordForm = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false)
@@ -39,12 +41,11 @@ const ForgotPasswordForm = () => {
 
   if (submitted) {
     return (
-      <Typography variant="body2">
-        Thanks! Your password reset request has been recieved.
-        <br/>
-        If an account
-        exists, you will recieve an email.
-      </Typography>
+      <TextAndCta
+        message="Thanks! Your password reset request has been recieved. If an account exists, you will recieve an email."
+        ctaText="Sign in"
+        ctaHref="/"
+      />  
     )
   }
 
@@ -58,17 +59,19 @@ const ForgotPasswordForm = () => {
         sx={{
           display: 'flex',
           flexDirection: 'column',
-          justifyContent: 'flex-start',
-          alignItems: 'stretch',
-          gap: 6,
+          gap: 2,
         }}
       >
+        <Typography variant="body1" component="h1">
+          Forgot password
+        </Typography>
         <Box
           sx={{
             display: 'flex',
             flexDirection: 'column',
             justifyContent: 'flex-start',
-            gap: 4,
+            alignItems: 'stretch',
+            gap: 6,
           }}
         >
           <Box
@@ -76,69 +79,78 @@ const ForgotPasswordForm = () => {
               display: 'flex',
               flexDirection: 'column',
               justifyContent: 'flex-start',
-              alignItems: 'stretch',
-              gap: 2,
-              '& *:first-of-type': {
-                marginTop: 0,
-              },
+              gap: 4,
             }}
           >
-            <Controller
-              name={EMAIL_ID}
-              control={control}
-              defaultValue=""
-              rules={{
-                required: FORM_MESSAGING.REQUIRED,
-                pattern: {
-                  value: EMAIL_REGEX_PATTERN,
-                  message: FORM_MESSAGING.VALID_EMAIL,
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+                alignItems: 'stretch',
+                gap: 2,
+                '& *:first-of-type': {
+                  marginTop: 0,
                 },
               }}
-              render={({ field, fieldState }) => (
-                <TextField
+            >
+              <Controller
+                name={EMAIL_ID}
+                control={control}
+                defaultValue=""
+                rules={{
+                  required: FORM_MESSAGING.REQUIRED,
+                  pattern: {
+                    value: EMAIL_REGEX_PATTERN,
+                    message: FORM_MESSAGING.VALID_EMAIL,
+                  },
+                }}
+                render={({ field, fieldState }) => (
+                  <TextField
+                    fullWidth
+                    {...field}
+                    disabled={isLoading}
+                    label={EMAIL_LABEL}
+                    type="email"
+                    variant="outlined"
+                    error={!!fieldState.error}
+                    helperText={fieldState.error?.message}
+                  />
+                )}
+              />
+              <Box>
+                <LoadingButton
                   fullWidth
-                  {...field}
-                  disabled={isLoading}
-                  label="Email"
-                  type="email"
-                  variant="outlined"
-                  error={!!fieldState.error}
-                  helperText={fieldState.error?.message}
-                />
-              )}
-            />
-            <Box>
-              <LoadingButton
-                fullWidth
-                loading={isLoading}
-                type="submit"
-                variant="contained"
-              >
-                Reset password
-              </LoadingButton>
-              {formError && (
-                <Typography variant="caption" color="error">
-                  {formError.message}
-                </Typography>
-              )}
+                  loading={isLoading}
+                  type="submit"
+                  variant="contained"
+                >
+                  Reset password
+                </LoadingButton>
+                {formError && (
+                  <Typography variant="caption" color="error">
+                    {formError.message}
+                  </Typography>
+                )}
+              </Box>
             </Box>
-          </Box>
-          <Divider />
-          <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'flex-start',
-              alignItems: 'flex-start',
-              gap: 2,
-            }}
-          >
-            <Typography variant="body1" component="h1">
-              Already know your password?
-            </Typography>
-            <Link href="/" passHref>
-              <Button variant="outlined" fullWidth>Sign in</Button>
-            </Link>
+            <Divider />
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+                alignItems: 'flex-start',
+                gap: 2,
+              }}
+            >
+              <Typography variant="body1" component="h1">
+                Already know your password?
+              </Typography>
+              <Link href="/" passHref>
+                <Button variant="outlined" fullWidth>Sign in</Button>
+              </Link>
+            </Box>
           </Box>
         </Box>
       </Box>
--- components/ForgotPasswordPage/index.tsx
+++ components/ForgotPasswordPage/index.tsx
@@ -1,9 +1,5 @@
 import Page from '../Page'
 import ForgotPasswordForm from '../ForgotPasswordForm'
-import { Box, Container } from '@mui/system'
-import Image from 'next/image'
-import Link from 'next/link'
-import { Typography } from '@mui/material'
 
 const ForgotPasswordPage = () => (
   <Page
@@ -12,31 +8,7 @@ const ForgotPasswordPage = () => (
     pageTitle="Forgot Password"
     thinContainer
   >
-    <Container maxWidth="xs">
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'flex-start',
-          gap: 2,
-        }}
-      >
-        <Link href="/" passHref>
-          <Box component="a">
-            <Image
-              alt="FAMELESS"
-              src="/fameless.svg"
-              height={26}
-              width={160}
-            />
-          </Box>
-        </Link>
-        <Typography variant="body1" component="h1">
-          Forgot password
-        </Typography>
-        <ForgotPasswordForm />
-      </Box>
-    </Container>
+    <ForgotPasswordForm />
   </Page>
 )
 
