ff1d8bd823d77184c8229d27d8de4505e2be4714
Chris Vince <chrisvince@gmail.com>
2022-10-18 14:43:48 -0400

    fix: category pill linking
    Removed post category pill linking if categories disabled

--- components/CategoryChip/index.tsx
+++ components/CategoryChip/index.tsx
@@ -2,26 +2,37 @@ import { Chip, Link as MuiLink } from '@mui/material'
 import Link from 'next/link'
 
 import truncateString from '../../utils/truncateString'
+import constants from '../../constants'
+
+const { CATEGORIES_ENABLED } = constants
 
 interface Props {
   name: string
   slug: string
 }
 
-const CategoryChip = ({ name, slug }: Props) => (
-  <Link href={`/category/${slug}`} passHref>
-    <MuiLink
-      underline="none"
-      sx={{ display: 'flex' }}
-    >
-      <Chip
-        label={truncateString(name)}
-        size="small"
-        variant="outlined"
-        clickable
-      />
-    </MuiLink>
-  </Link>
-)
+const CategoryChip = ({ name, slug }: Props) => {
+  const chipElement = (
+    <Chip
+      label={truncateString(name)}
+      size="small"
+      variant="outlined"
+      sx={{ cursor: CATEGORIES_ENABLED ? 'pointer' : 'inherit' }}
+      clickable={CATEGORIES_ENABLED}
+    />
+  )
+
+  if (!CATEGORIES_ENABLED) {
+    return chipElement
+  }
+
+  return (
+    <Link href={`/category/${slug}`} passHref>
+      <MuiLink underline="none" sx={{ display: 'flex' }}>
+        {chipElement}
+      </MuiLink>
+    </Link>
+  )
+}
 
 export default CategoryChip
