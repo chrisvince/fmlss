1d67b6823b4432eb109a91cecc9916ae24c27634
Chris Vince <chrisvince@gmail.com>
2023-04-06 00:13:51 -0400

    feat: create CloseButtonWrapper
    Create CloseButtonWrapper for wrapping contents and displaying a close
    button.

---
+++ components/CloseButtonWrapper/index.tsx
@@ -0,0 +1,77 @@
+import { CloseRounded } from '@mui/icons-material'
+import { ButtonBase } from '@mui/material'
+import { Box } from '@mui/system'
+
+interface Props {
+  children: React.ReactNode
+  onClose?: () => void
+  showCloseButton?: boolean
+}
+
+const CloseButtonWrapper = ({ children, onClose }: Props) => (
+  <Box
+    sx={{
+      position: 'relative',
+      display: 'block',
+      '@media (hover: none)': {
+        '.MuiButtonBase-root': {
+          opacity: 1,
+        },
+      },
+      '@media (hover: hover)': {
+        '&:hover': {
+          '.MuiButtonBase-root': {
+            opacity: 1,
+          },
+        },
+      },
+    }}
+  >
+    {onClose && (
+      <ButtonBase
+        onClick={onClose}
+        aria-label="Close"
+        sx={{
+          opacity: 0,
+          zIndex: 1,
+          position: 'absolute',
+          top: '-12px',
+          right: '-12px',
+          '@media (hover: none)': {
+            '.MuiBox-root': {
+              backgroundColor: 'grey.200',
+            },
+          },
+          '@media (hover: hover)': {
+            '&:hover': {
+              '.MuiBox-root': {
+                backgroundColor: 'grey.200',
+              },
+            },
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: '50%',
+            backgroundColor: 'grey.300',
+            padding: 0.4,
+          }}
+        >
+          <CloseRounded
+            sx={{
+              height: '16px',
+              width: '16px',
+            }}
+          />
+        </Box>
+      </ButtonBase>
+    )}
+    <Box>{children}</Box>
+  </Box>
+)
+
+export default CloseButtonWrapper
