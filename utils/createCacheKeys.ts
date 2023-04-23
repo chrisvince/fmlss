eb019f9357219cd5a927163ce87bb5effb326682
Chris Vince <chrisvince@gmail.com>
2023-04-24 07:26:39 +0930

    chore: cleanup createCacheKeys

--- utils/createCacheKeys.ts
+++ utils/createCacheKeys.ts
@@ -5,25 +5,32 @@ const createHashtagPostsCacheKey = (
   showType: 'post' | 'reply' | 'both',
   sortMode: FeedSortMode = 'latest',
   pageIndex: number | null = 0
-) => `hashtag/${slug}/posts/${showType}/${sortMode}${pageIndex === null ? '' : `-${pageIndex}`}`
+) =>
+  `hashtag/${slug}/posts/${showType}/${sortMode}${
+    pageIndex === null ? '' : `-${pageIndex}`
+  }`
 
 const createPostAuthorCacheKey = (slug: string) => `post/${slug}/author`
 
-const createPostCacheKey = (slug?: string | null) => slug ? `post/${slug}` : null
+const createPostCacheKey = (slug?: string | null) =>
+  slug ? `post/${slug}` : null
 
 const createPostFeedCacheKey = (
   sortMode: FeedSortMode,
   pageIndex: number | null = 0
-) => `post/feed/${sortMode}${pageIndex ===  null ? '' : `-${pageIndex}`}`
+) => `post/feed/${sortMode}${pageIndex === null ? '' : `-${pageIndex}`}`
 
-const getPageIndexFromCacheKey =
-  (cacheKey: string) => cacheKey.split('-').at(-1) as string
+const getPageIndexFromCacheKey = (cacheKey: string) =>
+  cacheKey.split('-').at(-1) as string
 
 const createPostRepliesCacheKey = (
   slug: string,
   pageIndex: number | null = 0,
   viewMode: 'start' | 'end' = 'start'
-) => `post/${slug}/replies${viewMode === 'end' ? '-reverse' : ''}${pageIndex === null ? '' : `-${pageIndex}`}`
+) =>
+  `post/${slug}/replies${viewMode === 'end' ? '-reverse' : ''}${
+    pageIndex === null ? '' : `-${pageIndex}`
+  }`
 
 const createPostLikeCacheKey = (postId: string, uid: string) =>
   `post/${postId}/like/${uid}`
@@ -51,14 +58,17 @@ const createCategoryPostsCacheKey = (
   slug: string,
   sortMode: FeedSortMode = 'latest',
   pageIndex: number | null = 0
-) => `category/${slug}/posts/${sortMode}${pageIndex === null ? '' : `-${pageIndex}`}`
+) =>
+  `category/${slug}/posts/${sortMode}${
+    pageIndex === null ? '' : `-${pageIndex}`
+  }`
 
 const createCategoryCacheKey = (slug: string) => `category/${slug}`
 
 const createMiniHashtagsCacheKey = () => 'hashtags/mini'
 const createMiniCategoriesCacheKey = () => 'categories/mini'
 
-const createUserCacheKey = (uid: string | null) => uid ? `user/${uid}` : null
+const createUserCacheKey = (uid: string | null) => (uid ? `user/${uid}` : null)
 
 const createCategoriesStartsWithCacheKey = (searchString: string) =>
   `categories/search/${searchString}`
