255264d83a1afef9d022630a89ab8f1d4333d90d
Chris Vince <chrisvince@gmail.com>
2023-06-29 16:08:29 +0200

    feat: update SectionHeading styles
    Also remove unused prop type.

--- components/SectionHeading/index.tsx
+++ components/SectionHeading/index.tsx
@@ -5,37 +5,19 @@ import MobileContainer from '../MobileContainer'
 
 interface PropTypes {
   children: string | React.ReactNode
-  sticky?: boolean
 }
 
 const SectionHeading = ({ children }: PropTypes) => {
   if (!children) return null
 
   return (
-    <Box
-      sx={{
-        backgroundColor: {
-          sm: 'background.paper',
-        },
-        marginBottom: {
-          xs: 1,
-          sm: 2,
-        },
-        borderBottom: {
-          sm: '1px solid',
-        },
-        borderColor: {
-          sm: 'divider',
-        },
-        paddingBottom: 0.5,
-        paddingTop: {
-          xs: 1,
-          sm: 0,
-        },
-      }}
-    >
+    <Box sx={{}}>
       <MobileContainer>
-        <Typography component="h1" variant="h6">
+        <Typography
+          component="h1"
+          variant="h4"
+          sx={{ fontWeight: 'bold', mb: 1 }}
+        >
           {children}
         </Typography>
       </MobileContainer>
