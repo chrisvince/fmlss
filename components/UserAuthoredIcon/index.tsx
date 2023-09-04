da16b7cc4fcdb3254fe1eaf7c66ef4f246e02142
Chris Vince <chrisvince@gmail.com>
2023-09-03 22:48:55 -0400

    feat: simplified UserAuthoredIcon tooltip

--- components/UserAuthoredIcon/index.tsx
+++ components/UserAuthoredIcon/index.tsx
@@ -2,10 +2,7 @@ import { PersonRounded } from '@mui/icons-material'
 import { Tooltip } from '@mui/material'
 
 const UserAuthoredIcon = () => (
-  <Tooltip
-    placement="top-start"
-    title="This post was created by you. Only you can see this."
-  >
+  <Tooltip placement="top-start" title="Created by you. Only you can see this.">
     <PersonRounded
       sx={{
         fontSize: '1em',
