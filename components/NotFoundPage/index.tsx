1d3aeb3facf0d622a887f1b35e8e18a75aac2c53
Chris Vince <chrisvince@gmail.com>
2023-12-12 09:14:52 -0500

    fix: 404 page layout issues
    Fix header line height, add mobile container.

--- components/NotFoundPage/index.tsx
+++ components/NotFoundPage/index.tsx
@@ -5,6 +5,7 @@ import SidebarHashtagsSection from '../SidebarHashtagsSection'
 import Page from '../Page'
 import TextAndCta from '../TextAndCta'
 import constants from '../../constants'
+import MobileContainer from '../MobileContainer'
 
 const { TOPICS_ENABLED } = constants
 
@@ -18,22 +19,28 @@ const NotFoundPage = () => (
       </>
     }
   >
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 4,
-      }}
-    >
-      <Typography component="h1" variant="h4">
-        We couldn&apos;t find that page. 🤔
-      </Typography>
-      <TextAndCta
-        ctaHref="/"
-        ctaText="Go back home"
-        message="It looks like the page you're looking for doesn't exist. Click below to go back home."
-      />
-    </Box>
+    <MobileContainer>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 4,
+        }}
+      >
+        <Typography
+          component="h1"
+          variant="h4"
+          sx={{ lineHeight: [undefined, '0.8em'] }}
+        >
+          We couldn&apos;t find that page. 🤔
+        </Typography>
+        <TextAndCta
+          ctaHref="/"
+          ctaText="Go back home"
+          message="It looks like the page you're looking for doesn't exist. Click below to go back home."
+        />
+      </Box>
+    </MobileContainer>
   </Page>
 )
 
