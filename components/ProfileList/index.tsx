98cabbf1ddd9ce81ac7411893c900c89d77e7965
Chris Vince <chrisvince@gmail.com>
2024-02-12 12:26:03 -0500

    fix: profile page scroll to alignment
    This fixes issue where the browser wouldn't scroll to the correct point
    on section scroll.

--- components/ProfileList/index.tsx
+++ components/ProfileList/index.tsx
@@ -1,40 +1,59 @@
-import { List, ListSubheader } from '@mui/material'
+import { List, ListSubheader, useTheme } from '@mui/material'
 import MobileContainer from '../MobileContainer'
 import { Box } from '@mui/system'
 
 interface ProfileListProps {
   children: React.ReactNode
-  heading: string
+  heading?: string
+  id?: string
 }
 
-const ProfileList = ({ children, heading }: ProfileListProps) => (
-  <Box sx={{ marginTop: 4 }}>
-    <MobileContainer>
-      <ListSubheader component="div" disableGutters>
-        {heading}
-      </ListSubheader>
-    </MobileContainer>
-    <List
+const ProfileList = ({ children, heading, id }: ProfileListProps) => {
+  const theme = useTheme()
+
+  return (
+    <Box
       sx={{
-        padding: 0,
-        '& .MuiListItemButton-root': {
-          borderRadius: 0,
-        },
-        '& > .MuiListItem-root, & > .MuiListItem-container': {
-          padding: 0,
-          borderBottom: '1px solid',
-          borderBottomColor: 'divider',
-        },
-        '& > .MuiListItem-root:first-of-type, & > .MuiListItem-container:first-of-type':
-          {
-            borderTop: '1px solid',
-            borderTopColor: 'divider',
+        marginTop: 4,
+        [theme.breakpoints.up('sm')]: {
+          '&::before': {
+            content: "''",
+            display: 'block',
+            height: '95px',
+            marginTop: '-95px',
+            visibility: 'hidden',
           },
+        },
       }}
+      id={id}
     >
-      {children}
-    </List>
-  </Box>
-)
+      <MobileContainer>
+        <ListSubheader component="div" disableGutters>
+          {heading}
+        </ListSubheader>
+      </MobileContainer>
+      <List
+        sx={{
+          padding: 0,
+          '& .MuiListItemButton-root': {
+            borderRadius: 0,
+          },
+          '& > .MuiListItem-root, & > .MuiListItem-container': {
+            padding: 0,
+            borderBottom: '1px solid',
+            borderBottomColor: 'divider',
+          },
+          '& > .MuiListItem-root:first-of-type, & > .MuiListItem-container:first-of-type':
+            {
+              borderTop: '1px solid',
+              borderTopColor: 'divider',
+            },
+        }}
+      >
+        {children}
+      </List>
+    </Box>
+  )
+}
 
 export default ProfileList
