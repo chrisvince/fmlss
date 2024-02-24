74adcb225a3ce5d3ef093b5c79bbdde645f2c29b
Chris Vince <chrisvince@gmail.com>
2024-02-24 11:27:44 -0500

    chore: remove logic based on topic flag

--- components/TopicBadge/index.tsx
+++ components/TopicBadge/index.tsx
@@ -1,13 +1,9 @@
-import { Chip, Link as MuiLink, Tooltip } from '@mui/material'
+import { Link as MuiLink, Tooltip } from '@mui/material'
 import Link from 'next/link'
 
-import truncateString from '../../utils/truncateString'
-import constants from '../../constants'
 import TruncatedPathTitle from '../TruncatedPathTitle'
 import TopicPathTitleText from '../TopicPathTitleText'
 import { SubtopicSegment } from '../../types'
-
-const { TOPICS_ENABLED } = constants
 
 interface Props {
   pathTitle: string
@@ -15,46 +11,27 @@ interface Props {
   subtopicSegments: SubtopicSegment[]
 }
 
-const TopicBadge = ({ pathTitle, slug, subtopicSegments }: Props) => {
-  const chipElement = (
-    <Chip
-      label={truncateString(pathTitle)}
-      size="small"
-      variant="outlined"
+const TopicBadge = ({ pathTitle, slug, subtopicSegments }: Props) => (
+  <Tooltip placement="bottom" title={pathTitle}>
+    <MuiLink
+      component={Link}
+      href={`/topic/${slug}`}
       sx={{
-        cursor: TOPICS_ENABLED ? 'pointer' : 'inherit',
-        height: '20px',
+        display: 'flex',
+        height: '1em',
+        '&:hover': {
+          borderBottom: '1px solid currentColor',
+        },
       }}
-      clickable={TOPICS_ENABLED}
-    />
-  )
-
-  if (!TOPICS_ENABLED) {
-    return chipElement
-  }
-
-  return (
-    <Tooltip placement="bottom" title={pathTitle}>
-      <MuiLink
-        component={Link}
-        href={`/topic/${slug}`}
-        sx={{
-          display: 'flex',
-          height: '1em',
-          '&:hover': {
-            borderBottom: '1px solid currentColor',
-          },
-        }}
-        underline="none"
-        variant="caption"
-        color="text.secondary"
-      >
-        <TopicPathTitleText>
-          <TruncatedPathTitle subtopicSegments={subtopicSegments} />
-        </TopicPathTitleText>
-      </MuiLink>
-    </Tooltip>
-  )
-}
+      underline="none"
+      variant="caption"
+      color="text.secondary"
+    >
+      <TopicPathTitleText>
+        <TruncatedPathTitle subtopicSegments={subtopicSegments} />
+      </TopicPathTitleText>
+    </MuiLink>
+  </Tooltip>
+)
 
 export default TopicBadge
