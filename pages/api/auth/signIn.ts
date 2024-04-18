0e442d8e8509af93697a31262635631febfd4032
Chris Vince <chrisvince@gmail.com>
2024-04-17 23:49:59 -0400

    chore: return error on auth

--- pages/api/auth/signIn.ts
+++ pages/api/auth/signIn.ts
@@ -6,7 +6,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     await setAuthCookies(req, res)
   } catch (error) {
-    return res.status(500).json({ error: 'Unexpected error.' })
+    return res.status(500).json({ error })
   }
   return res.status(200).json({ success: true })
 }
--- pages/api/auth/signOut.ts
+++ pages/api/auth/signOut.ts
@@ -4,8 +4,8 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     await unsetAuthCookies(req, res)
-  } catch (e) {
-    return res.status(500).json({ error: 'Unexpected error.' })
+  } catch (error) {
+    return res.status(500).json({ error })
   }
   return res.status(200).json({ success: true })
 }
