02fe0fb2e1d517c992512b2226d4f33b4288ce8b
Chris Vince <chrisvince@gmail.com>
2024-05-13 17:53:32 -0400

    feat: remove feed fetching server side
    This is to hopefully speed up loading of feed. Since the feed isn't used
    for indexing, it can be fetched server side and will appear to load
    quicker for the user.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,13 @@ import { SWRConfig } from 'swr'
 
 import FeedPage from '../components/FeedPage'
 import { FeedSortMode } from '../types'
-import {
-  createPostFeedSWRGetKey,
-  createUserCacheKey,
-} from '../utils/createCacheKeys'
+import { createUserCacheKey } from '../utils/createCacheKeys'
 import constants from '../constants'
 import isInternalRequest from '../utils/isInternalRequest'
 import getSidebarDataServer from '../utils/data/sidebar/getSidebarDataServer'
-import getPostFeedServer from '../utils/data/posts/getPostFeedServer'
 import getUserDataServer from '../utils/data/user/getUserDataServer'
 import { GetServerSideProps } from 'next'
 import getUidFromCookies from '../utils/auth/getUidFromCookies'
-import { unstable_serialize } from 'swr/infinite'
 
 const { GET_SERVER_SIDE_PROPS_TIME_LABEL } = constants
 
@@ -42,11 +37,6 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     }
   }
 
-  const postFeedCacheKey = createPostFeedSWRGetKey({
-    sortMode: FeedSortMode.Latest,
-    uid,
-  })
-
   const userCacheKey = createUserCacheKey(uid)
   const sidebarDataPromise = getSidebarDataServer()
 
@@ -61,11 +51,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     }
   }
 
-  const [posts, sidebarFallbackData, userData] = await Promise.all([
-    getPostFeedServer({
-      sortMode: FeedSortMode.Latest,
-      uid,
-    }),
+  const [sidebarFallbackData, userData] = await Promise.all([
     sidebarDataPromise,
     getUserDataServer(uid),
   ])
@@ -75,7 +61,6 @@ export const getServerSideProps: GetServerSideProps = async ({ req }) => {
   return {
     props: {
       fallback: {
-        [unstable_serialize(postFeedCacheKey)]: [posts],
         [userCacheKey]: userData,
         ...sidebarFallbackData,
       },
