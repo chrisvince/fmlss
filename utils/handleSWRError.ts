b246fddd66761e8d1d84db187259f03158be5142
Chris Vince <chrisvince@gmail.com>
2024-05-13 19:55:40 -0400

    chore: improve SWR error reporting
    Add context and key to error reporting.

--- utils/handleSWRError.ts
+++ utils/handleSWRError.ts
@@ -1,14 +1,20 @@
-import { captureException } from '@sentry/nextjs'
+import { captureException, withScope } from '@sentry/nextjs'
+import { BareFetcher, PublicConfiguration } from 'swr/dist/_internal'
 
-const handleSWRError = (error: any) => {
+const handleSWRError = (
+  error: any,
+  key: string,
+  config: Readonly<PublicConfiguration<any, any, BareFetcher<any>>>
+) => {
   if (error.status === 403 || error.status === 404) {
     return
   }
 
-  captureException(error, {
-    tags: {
-      source: 'useSWR',
-    },
+  withScope(scope => {
+    scope.setTag('key', key)
+    scope.setTag('source', 'useSWR')
+    scope.setContext('SWR Config', config)
+    captureException(error)
   })
 }
 
